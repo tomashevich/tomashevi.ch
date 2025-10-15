4e96acd1716340e4ac1a9a1e3d5a62a610b120eb
tomashevich <110192686+tomashevich@users.noreply.github.com>
2025-10-15 16:11:15 +0900

    perf: added sqlite3 pragmas

--- server/database/database.go
+++ server/database/database.go
@@ -2,6 +2,7 @@ package database
 
 import (
 	"database/sql"
+	"log"
 
 	_ "github.com/mattn/go-sqlite3"
 )
@@ -20,8 +21,18 @@ func NewDatabase(database string) (*Database, error) {
 		return nil, err
 	}
 
-	// create tables
-	// TODO: enable PRAGMAs. refactor var tables to INIT exec
+	pragmas := []string{
+		"PRAGMA foreign_keys = ON",
+		"PRAGMA journal_mode = WAL",
+		"PRAGMA synchronous = NORMAL",
+		"PRAGMA temp_store = MEMORY",
+	}
+	for _, pragma := range pragmas {
+		if _, err := db.Exec(pragma); err != nil {
+			log.Printf("pragma %s has error %s", pragma, err.Error())
+		}
+	}
+
 	tables := []string{
 		"CREATE TABLE IF NOT EXISTS souls (id INTEGER PRIMARY KEY, address VARCHAR(39) NOT NULL UNIQUE, seed VARCHAR(32), painted_pixels INTEGER NOT NULL DEFAULT 0)",
 		"CREATE TABLE IF NOT EXISTS pixels (soul_id INTEGER NOT NULL REFERENCES souls(id), color TEXT NOT NULL, x INT NOT NULL, y INT NOT NULL, PRIMARY KEY (x, y))",
